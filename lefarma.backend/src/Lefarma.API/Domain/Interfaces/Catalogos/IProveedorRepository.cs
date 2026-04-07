2cd83fa0557146ed0521f4f9d1a3cc6e0c4f59a6
Brandom Ledesma <brandom.ledesma@hotmail.com>
2026-04-06 18:17:28 -0600

    feat(domain): agrega entidad ProveedorDetalle y actualiza interfaces
    - Agrega entidad ProveedorDetalle para datos de contacto
    - Actualiza Proveedor entity con nuevas propiedades
    - Modifica IProveedorRepository con nuevos métodos

--- lefarma.backend/src/Lefarma.API/Domain/Interfaces/Catalogos/IProveedorRepository.cs
+++ lefarma.backend/src/Lefarma.API/Domain/Interfaces/Catalogos/IProveedorRepository.cs
@@ -1,8 +1,9 @@
 using Lefarma.API.Domain.Entities.Catalogos;
 using Lefarma.API.Domain.Interfaces;
 
-namespace Lefarma.API.Domain.Interfaces.Catalogos {
+namespace Lefarma.API.Domain.Interfaces.Catalogos;
+
 public interface IProveedorRepository : IBaseRepository<Proveedor>
-    {
-    }
+{
+    Task<Proveedor?> GetByIdWithDetailsAsync(int id);
 }
